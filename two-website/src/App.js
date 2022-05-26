6ed2e991c2b53936f0f5ca68b7de5879d0e94456
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-05-26 23:00:02 +0530

    website template convert into react template

---
+++ two-website/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import Navbar from "./Navbar";
+
+
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <HeroSection/>
+            <AppSection/>
+            <CardSection/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App;
